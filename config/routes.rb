02d1c010204945c795773e6b72f8062d94a1809f
Jcg408 <jcg321@comcas.net>
2019-03-13 12:26:15 -0400

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/hello_world'
+
+  root 'welcome#hello_world'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
