703204a12fa61ac1d2c6cc53931769c57b3a9754
Bernadette Davis <bsd231@nyu.edu>
2019-11-12 00:43:32 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/hello_world' => 'hw#hello_world'
+  #url path is /hello_world, directory path is hw#hello_world
+
+
 end
